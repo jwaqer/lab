be2eff8619ef0f7ba80befddd8520c6bf8bee827
jwaqer <99970163+jwaqer@users.noreply.github.com>
2022-03-11 23:52:59 +0800

    Update and rename Notebook.java to NoteBook.java

--- src/base/NoteBook.java
+++ src/base/NoteBook.java
@@ -1,19 +1,27 @@
 package base;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Notebook {
+
+public class NoteBook {
 	private ArrayList<Folder> folders;
 	
-	public Notebook() {
-		folders=new ArrayList<Folder>();
+	public NoteBook() {
+		folders = new ArrayList<Folder>();
 	}
 	
 	public boolean createTextNote(String folderName, String title){
-		TextNote note=new TextNote(title);
+		TextNote note= new TextNote(title);
 		return insertNote(folderName, note);
 	}
-	
+
+	public boolean createTextNote(String folderName,String title,String content){
+		TextNote note = new TextNote(title, content);
+		return insertNote(folderName, note);
+	}
+
 	public boolean createImageNote(String folderName, String title){
 		ImageNote note = new ImageNote(title);
 		return insertNote(folderName, note);
@@ -30,16 +38,17 @@ public class Notebook {
 		for (Folder f1:folders) {
 			if(f1.getName() == folderName) {
 				f = f1;		
+				break;
 			}
 		}
 		if(f == null) {
 			f = new Folder(folderName);
 			folders.add(f);
-			
+		
 		}
 		
-		for(Note n:f.getNotes()) {
-			if(f.equals(note)) {
+		for(Note n : f.getNotes()) {
+			if(n.equals(note)) {
 				System.out.println("Creating note " + note.getTitle() + " under folder " + folderName + " failed");
 				return false;
 			}
@@ -51,5 +60,24 @@ public class Notebook {
 		
 	}
 	
+	public void sortFolders() {
+		for (Folder f : folders) {
+			f.sortNotes();}
+
+		Collections.sort(folders);
+
+
+	}
 	
+	public List<Note> searchNotes (String keywords) {
+
+		ArrayList<Note> search_result = new ArrayList<Note>();
+        for (Folder f : folders){
+            search_result.addAll( f.searchNotes(keywords) ); }
+        return search_result;
+
+
+	}
+
+
 }
