e3288604411fc7b12aada2f3716a992e956c9747
jwaqer <99970163+jwaqer@users.noreply.github.com>
2022-03-19 00:00:03 +0800

    Update NoteBook.java

--- src/base/NoteBook.java
+++ src/base/NoteBook.java
@@ -4,14 +4,39 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import java.io.Serializable;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-public class NoteBook {
+public class NoteBook implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
 	private ArrayList<Folder> folders;
+
+	
 	
 	public NoteBook() {
 		folders = new ArrayList<Folder>();
 	}
 	
+	public NoteBook(String file){
+			
+		FileInputStream fis = null;
+		ObjectInputStream in = null;
+		try {
+			fis = new FileInputStream(file);
+			in = new ObjectInputStream(fis);
+			NoteBook n = (NoteBook) in.readObject();
+			folders = n.folders;
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public boolean createTextNote(String folderName, String title){
 		TextNote note= new TextNote(title);
 		return insertNote(folderName, note);
@@ -79,5 +104,27 @@ public class NoteBook {
 
 	}
 
+	
+	public boolean save(String file){
+		
+
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+
+		try {
+		
+			fos = new FileOutputStream(file);
+			out = new ObjectOutputStream(fos);
+			out.writeObject(this);
+			out.close();
+		} catch (Exception e) {
+		   return false;
+		}
+		return true;
+	}
+
+	
+
+	
 
 }
