8c57fb4c316796fee2c65b1d52954b5dd61d6742
jwaqer <99970163+jwaqer@users.noreply.github.com>
2022-03-18 23:59:49 +0800

    Update TextNote.java

--- src/base/TextNote.java
+++ src/base/TextNote.java
@@ -1,5 +1,18 @@
 package base;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 
 public class TextNote extends Note{
@@ -16,6 +29,45 @@ public class TextNote extends Note{
 	
 	public String getContent(){
         return content;
-    }
+	}
+	
+	public TextNote(File f) {
+		super(f.getName());
+		this.content = getTextFromFile(f.getAbsolutePath());
+		}
+	
+	
+	private String getTextFromFile(String absolutePath) {
+		String result = "";
+		// TODO
+		
+			FileInputStream fis = null;
+			ObjectInputStream in = null;
+			try {
+				fis = new FileInputStream(absolutePath);
+				in = new ObjectInputStream(fis);
+				in.close();
+				result = (String) in.readObject();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+		return result;
+	
+	}
+
+	public void exportTextToFile(String pathFolder) {
+		//TODO
+		
+		
+
+		
+		// TODO
+		
+		
+		
+		}
+		
+		
 }
 
