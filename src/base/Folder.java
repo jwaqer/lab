2307102fd1c07fb90028355556794b0ccd0707c1
jwaqer <99970163+jwaqer@users.noreply.github.com>
2022-03-19 00:01:27 +0800

    Update Folder.java

--- src/base/Folder.java
+++ src/base/Folder.java
@@ -1,106 +1,131 @@
 package base;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Stack;
 
-public class Folder implements Comparable<Folder>{
 
+public class Folder implements Comparable<Folder>, Serializable{
 	private ArrayList<Note> notes;
 	private String name;
-	
+	private static final long serialVersionUID = 2L;
+
 	public Folder(String name) {
 		notes = new ArrayList<Note>();
 		this.name = name;
 	}
-	
+
 	public void addNote(Note e) {
 		notes.add(e);
 	}
-	
+
+
 	public ArrayList<Note> getNotes() {
 		return notes;
 	}
-	
+
+
 	public String getName() {
 		return name;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(name);
 	}
-	
 
-	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (!(obj instanceof Folder))
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
 			return false;
 		Folder other = (Folder) obj;
 		return Objects.equals(name, other.name);
 	}
-	
-	
-	@Override
+
 	public String toString() {
 		int nText = 0;
 		int nImage = 0;
-		
-		for (Note note: notes) {
-			if (note instanceof TextNote) {
+
+		for(Note n : notes) {
+
+			if(n instanceof TextNote) {
 				nText++;
-			}
-			if (note instanceof ImageNote) {
+			} else if(n instanceof ImageNote) {
 				nImage++;
 			}
 		}
-		
+
 		return name + ":" + nText + ":" + nImage;
 	}
 
+	// Lab3 Start
 	@Override
-	public int compareTo(Folder o) {
-	
-		return name.compareTo(o.name);
+	public int compareTo(Folder f) {
+		return name.compareTo(f.getName());
 	}
-		
-	public void sortNotes(){
+
+	public void sortNotes() {
 		Collections.sort(notes);
-
 	}
-	
 
-	public List<Note> searchNotes(String keywords){
-		
-		ArrayList<Note> list = new ArrayList<Note> ();
-		String[] search  = keywords.split(" ");
-		search = keywords.toLowerCase().split("or");
-		for(Note note: notes){
-			for(String s :search){
-				if(note instanceof ImageNote){
-					if (note.getTitle().toLowerCase().contains(s)){
-						list.add(note);	
-                    }
-				}
 
-				else if (note instanceof TextNote){
-                    if (((TextNote)note).getContent().toLowerCase().contains(s)){
-						if (note.getTitle().toLowerCase().contains(s)){
-							list.add(note);	
-						}
+	public List<Note> searchNotes(String keywords) {
+		List<Note> res = new ArrayList<Note>();
+
+		ArrayList<ArrayList<String>> patterns = new ArrayList<ArrayList<String>>();
+
+		//process keywords
+		String[] keyList = keywords.split(" ");
+		int i=0;
+		while(i<keyList.length) {
+			if(keyList[i].toLowerCase().equals("or")) {
+				i++;
+				patterns.get(patterns.size() - 1).add(keyList[i].toLowerCase());
+			} else {
+				ArrayList<String> newArr = new ArrayList<String>();
+				newArr.add(keyList[i].toLowerCase());
+				patterns.add(newArr);
+			}
+			i++;
+		}
+
+		for(Note n : notes) {
+			String toBeSearched = n.getTitle();
+			if(n instanceof TextNote) {
+				toBeSearched +=  ((TextNote)n).getContent();
+			}
+			toBeSearched = toBeSearched.toLowerCase();
+
+			boolean flag = true;
+			for(ArrayList<String> pattern : patterns) {
+				boolean flag2 = false;
+				for(String oneKey : pattern) {
+					if(toBeSearched.contains(oneKey)) {
+						flag2 = true;
+						break;
 					}
 				}
+				if(!flag2) {
+					flag = false;
+					break;
+				}
 			}
+			if(flag) res.add(n);
 		}
-		return list;
-		
-		
-		
+
+		return res;
 	}
-	
+
+
+
+
 
 }
